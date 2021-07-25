ca73d62620a47193b8e3c0a5cb42ef94a9915133
AllisonAnz <AllisonAnzalone@gmail.com>
2021-07-25 11:51:58 -0500

    Done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,158 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Switch, Route, Link, BrowserRouter as Router } from "react-router-dom";
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import AddProduct from './components/AddProduct';
+import Cart from './components/Cart';
+import ProductList from './components/ProductList';
+
+import Context from "./Context";
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: null,
+      cart: {},
+      products: []
+    };
+    this.routerRef = React.createRef();
+  }
+
+  getData() {
+    fetch('http://localhost:3001/products')
+      .then(res => res.json())
+      .then(data => this.setState({ products: data }))
+  }
+
+  componentDidMount() {
+    this.getData();
+  }
+
+
+  addProduct = (product, callback) => {
+    let products = this.state.products.slice();
+    products.push(product);
+    this.setState({ products }, () => callback && callback());
+  };
+
+  addToCart = cartItem => {
+    let cart = this.state.cart;
+    if (cart[cartItem.id]) {
+      cart[cartItem.id].amount += cartItem.amount;
+    } else {
+      cart[cartItem.id] = cartItem;
+    }
+    if (cart[cartItem.id].amount > cart[cartItem.id].product.stock) {
+      cart[cartItem.id].amount = cart[cartItem.id].product.stock;
+    }
+    localStorage.setItem("cart", JSON.stringify(cart));
+    this.setState({ cart });
+  };
+
+  removeFromCart = cartItemId => {
+    let cart = this.state.cart;
+    delete cart[cartItemId];
+    localStorage.setItem("cart", JSON.stringify(cart));
+    this.setState({ cart });
+  };
+
+  clearCart = () => {
+    let cart = {};
+    localStorage.removeItem("cart");
+    this.setState({ cart });
+  };
+
+  checkout = () => {
+    const cart = this.state.cart;
+
+    const products = this.state.products.map(p => {
+      if (cart[p.name]) {
+        p.stock = p.stock - cart[p.name].amount;
+
+        const requestOptions = {
+          method: "PUT",
+          headers: { 'Content-Type': 'application/json'},
+          body: JSON.stringify({...p })
+        }
+
+        fetch(`http://localhost:3001/products/${p.id}`, requestOptions)
+        .then(res => res.json())
+        .then(product => 
+          this.setState({product}))
+          this.clearCart()
+     }
+     return p;
+   });
+
+    this.setState({ products });
+  };
+
+  render() {
+    return (
+      <Context.Provider
+        value={{
+          ...this.state,
+          removeFromCart: this.removeFromCart,
+          addToCart: this.addToCart,
+          addProduct: this.addProduct,
+          clearCart: this.clearCart,
+          checkout: this.checkout
+        }}
+      >
+        <Router ref={this.routerRef}>
+          <div className="App">
+            <nav
+              className="navbar-container"
+              role="navigation"
+            >
+              <div className="navbar-brand hero is-link">
+                <b className="navbar-item is-size-4 ">ecommerce</b>
+                <label
+                  role="button"
+                  className="navbar-burger burger"
+                  aria-label="menu"
+                  aria-expanded="false"
+                  data-target="navbarBasicExample"
+                  onClick={e => {
+                    e.preventDefault();
+                    this.setState({ showMenu: !this.state.showMenu });
+                  }}
+                >
+                  <span aria-hidden="true"></span>
+                  <span aria-hidden="true"></span>
+                  <span aria-hidden="true"></span>
+                </label>
+              </div>
+              <div className={`navbar-menu ${this.state.showMenu ? "is-active" : ""
+                }`}>
+                <Link to="/products" className="navbar-item">
+                  Products
+                </Link>
+                  <Link to="/add-product" className="navbar-item">
+                    Add Product
+                  </Link>
+                <Link to="/cart" className="navbar-item">
+                  Cart
+                  <span
+                    className="tag is-primary"
+                    style={{ marginLeft: "5px" }}
+                  >
+                    {Object.keys(this.state.cart).length}
+                  </span>
+                </Link>
+                
+              </div>
+            </nav>
+            <Switch>
+              <Route exact path="/" component={ProductList} />
+              <Route exact path="/cart" component={Cart} />
+              <Route exact path="/add-product" component={AddProduct} />
+              <Route exact path="/products" component={ProductList} />
+            </Switch>
+          </div>
+        </Router>
+      </Context.Provider>
+    );
+  }
 }
-
-export default App;
---
+++ src/components/AddProduct.js
@@ -0,0 +1,153 @@
+import React, { Component } from "react";
+import withContext from "../withContext";
+
+const initState = {
+    name: "",
+    price: "",
+    stock: "",
+    shortDesc: "",
+    description: "",
+    img: ""
+};
+
+class AddProduct extends Component {
+    constructor(props) {
+        super(props);
+        this.state = initState;
+    }
+
+
+    save = async (e) => {
+        e.preventDefault();
+        const { name, price, stock, shortDesc, description, img} = this.state;
+        if (name && price) {
+            
+        fetch('http://localhost:3001/products', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                name, price, stock, shortDesc, description, img
+            })
+        })
+            .then(res => res.json())
+            .then(data => 
+                this.props.context.addProduct(data), this.setState(() => this.initState)
+                );
+                
+            this.setState(
+                { flash: { status: 'is-success', msg: 'Product created successfully' } }
+            );
+
+        } else {
+            this.setState(
+                { flash: { status: 'is-danger', msg: 'Please enter name and price' } }
+            )
+        }
+    }
+        
+    
+
+    handleChange = e => this.setState({ [e.target.name]: e.target.value, error: "" });
+
+    render() {
+        const { name, price, stock, shortDesc, description, img } = this.state;
+        return(
+            <>
+                <div className="hero is-primary ">
+                    <div className="hero-body container">
+                        <h4 className="title">Add Product</h4>
+                    </div>
+                </div>
+                <br />
+                <br />
+                <form onSubmit={this.save}>
+                    <div className="columns is-mobile is-centered">
+                        <div className="column is-one-third">
+                            <div className="field">
+                                <label className="label">Product Name: </label>
+                                <input
+                                    className="input"
+                                    type="text"
+                                    name="name"
+                                    value={name}
+                                    onChange={this.handleChange}
+                                    required
+                                />
+                            </div>
+                            <div className="field">
+                                <label className="label">Price: </label>
+                                <input
+                                    className="input"
+                                    type="number"
+                                    name="price"
+                                    value={price}
+                                    onChange={this.handleChange}
+                                    required
+                                />
+                            </div>
+                            <div className="field">
+                                <label className="label">Available in Stock: </label>
+                                <input
+                                    className="input"
+                                    type="number"
+                                    name="stock"
+                                    value={stock}
+                                    onChange={this.handleChange}
+                                />
+                            </div>
+                            <div className="field">
+                                <label className="label">Short Description: </label>
+                                <input
+                                    className="input"
+                                    type="text"
+                                    name="shortDesc"
+                                    value={shortDesc}
+                                    onChange={this.handleChange}
+                                />
+                            </div>
+                            <div className="field">
+                                <label className="label">Description: </label>
+                                <textarea
+                                    className="textarea"
+                                    type="text"
+                                    rows="2"
+                                    style={{ resize: "none" }}
+                                    name="description"
+                                    value={description}
+                                    onChange={this.handleChange}
+                                />
+                            </div>
+                            <div className="field">
+                                <label className="label">Image Link: </label>
+                                <textarea
+                                    className="input"
+                                    type="text"
+                                    style={{ resize: "none" }}
+                                    name="img"
+                                    value={img}
+                                    onChange={this.handleChange}
+                                />
+                            </div>
+                            {this.state.flash && (
+                                <div className={`notification ${this.state.flash.status}`}>
+                                    {this.state.flash.msg}
+                                </div>
+                            )}
+                            <div className="field is-clearfix">
+                                <button
+                                    className="button is-primary is-outlined is-pulled-right"
+                                    type="submit"
+                                    onClick={this.save}
+                                >
+                                    Submit
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </>
+        );
+    }
+}
+
+export default withContext(AddProduct);
